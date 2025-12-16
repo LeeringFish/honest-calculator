a0ed41d13ddd8e16339ffa212312d168d25c3fc4
LeeringFish <theleeringfish@proton.me>
2025-12-15 20:06:57 -0500

    add performOperation method

--- src/honestcalculator/Main.java
+++ src/honestcalculator/Main.java
@@ -9,8 +9,10 @@ public class Main {
         String msg0 = "Enter an equation";
         String msg1 = "Do you even know what numbers are? Stay focused!";
         String msg2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?";
-        String calc, x, y, oper;
+        String msg3 = "Yeah... division by zero. Smart move...";
+        String calc, oper, xString, yString;
         String[] parts;
+        double x, y;
 
         while (true) {
             System.out.println(msg0);
@@ -21,23 +23,44 @@ public class Main {
             }
 
             parts = calc.split(" ");
-            x = parts[0];
+            xString = parts[0];
             oper = parts[1];
-            y = parts[2];
+            yString = parts[2];
 
-            if (notANumber(x) || notANumber((y))) {
+            if (notANumber(xString) || notANumber(yString)) {
                 System.out.println(msg1);
             } else if (!oper.matches("[-+*/]")) {
                 System.out.println(msg2);
             } else {
-                break;
+                x = Double.parseDouble(xString);
+                y = Double.parseDouble(yString);
+
+                if ("/".equals(oper) && y == 0) {
+                    System.out.println(msg3);
+                } else {
+                    System.out.println(performOperation(x, y, oper));
+                }
             }
-
         }
-
     }
 
     public static boolean notANumber(String str) {
         return !str.matches("-?\\d+(\\.\\d+)?");
     }
+
+    public static double performOperation(double x, double y, String oper) {
+        double result = 0;
+        if ("+".equals(oper)) {
+            result = x + y;
+        } else if ("-".equals(oper)) {
+            result = x - y;
+        } else if ("*".equals(oper)) {
+            result = x * y;
+        } else if ("/".equals(oper)) {
+            result = x / y;
+        }
+
+        return result;
+    }
+
 }
